e26232689d9a3c8222df84e60258409a95298796
sis061 <sis061@naver.com>
2024-01-26 23:17:20 +0900

    second commit

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.use(cors());
 
